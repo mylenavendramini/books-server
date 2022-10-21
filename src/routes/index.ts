a24a76fbc30d71cd409817d61c5f043df7057a2b
mylenavendramini <myvendramini@gmail.com>
2022-10-21 21:13:20 +0100

    route index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import {
   getBooks,
   updateBook,
   updateBackBook,
+  editBook,
 } from "../controllers/books/index";
 
 const router: Router = Router();
@@ -13,6 +14,7 @@ router.get("/", getBooks);
 router.post("/create-book", createBook);
 router.put("/update-book/:id", updateBook);
 router.put("/update-back-book/:id", updateBackBook);
+router.put("/edit-book/:id", editBook);
 router.put("/delete-book/:id", deleteBookFromPage);
 // router.delete("/delete-book/:id", deleteBook);
 
