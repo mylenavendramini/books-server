5d4d98d1fb8771067f5e1c15058fc84b649a6336
mylenavendramini <myvendramini@gmail.com>
2022-12-23 20:20:29 +0000

    routes index.ts

--- dist/js/routes/index.js
+++ dist/js/routes/index.js
@@ -7,6 +7,7 @@ router.get("/", index_1.getBooks);
 router.post("/create-book", index_1.createBook);
 router.put("/update-book/:id", index_1.updateBook);
 router.put("/update-back-book/:id", index_1.updateBackBook);
+router.put("/edit-book/:id", index_1.editBook);
 router.put("/delete-book/:id", index_1.deleteBookFromPage);
 // router.delete("/delete-book/:id", deleteBook);
 exports.default = router;
